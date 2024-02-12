d144efc804d80931ee488e7b0843a25a6aeef640
CynthiaRigo <73132695+CynthiaRigo@users.noreply.github.com>
2024-02-12 08:48:31 -0700

    Update zena_streamlit.py

--- zena_streamlit.py
+++ zena_streamlit.py
@@ -1,13 +1,18 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
-streamlit.title ('My Parents New Healthy Dine') 
+streamlit.title('My Parents New Healthy Diner')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:") 
-streamlit.text(my_data_row)
-  
+streamlit.header('Breakfast Menu') 
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal') 
+streamlit.text(' 🥗  Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 🍞 Avocado Toast') 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+ 
  
  
